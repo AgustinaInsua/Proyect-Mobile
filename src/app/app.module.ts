65ba4992c055a667c0fd3a08a02dd0c252eab0ba
Leandro Fernandez <leanfernandez97@gmail.com>
2023-02-01 08:51:16 -0300

    Start proyect mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports:[ BrowserModule, 
+            IonicModule.forRoot(), 
+            AppRoutingModule,
+            InputTextModule
+          ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/login/login.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { LoginPageRoutingModule } from './login-routing.module';
+
+import { LoginPage } from './login.page';
+import {InputTextModule} from 'primeng/inputtext';
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    LoginPageRoutingModule,
+    InputTextModule
+  ],
+  declarations: [LoginPage]
+})
+export class LoginPageModule {}

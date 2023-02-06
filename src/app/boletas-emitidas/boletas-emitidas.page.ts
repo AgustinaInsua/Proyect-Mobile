3e762c3938188425aa66478eb3a8e6642e64b295
facundo <aguss.insua99@gmail.com>
2023-02-06 11:58:43 -0300

    se agrega el enviroment

--- src/app/boletas-emitidas/boletas-emitidas.page.ts
+++ src/app/boletas-emitidas/boletas-emitidas.page.ts
@@ -1,7 +1,10 @@
+import { BoletasService } from './../service/boleta-service/boletas.service';
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/boleta-service/api-service/api.service';
 
 import { Boleta } from './boleta';
 import { BOLETAS } from './mock-boletas';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-boletas-emitidas',
@@ -10,14 +13,32 @@ import { BOLETAS } from './mock-boletas';
 })
 export class BoletasEmitidasPage implements OnInit {
 
-  boletas= BOLETAS;
+  boletas: any;
   selectedBoleta!: Boleta;
-  constructor() { }
+  urlGetBoletas : any;
+  boleta = new Boleta();
+  constructor(private apiService: ApiService, private boletasService: BoletasService) { }
 
   ngOnInit() {
-   
+    this.urlGetBoletas = this.boletasService.apiURL;
+    this.boletas = this.urlGetBoletas;
   }
+
   onSelect(boleta: Boleta): void {
     this.selectedBoleta = boleta;
+  }
+
+  get(){
+    this.returnResponse(this.apiService.get(this.urlGetBoletas));
+  }
+
+  returnResponse(obs: Observable<any>){
+    obs.subscribe( {next: (response: any) => {   
+      console.log(response);
+    },
+    error: (error: { message: any; }) =>{
+      console.log(error.message);
+    }
+   })
   }
 }
--- src/app/service/boleta-service/boletas.service.ts
+++ src/app/service/boleta-service/boletas.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BoletasService {
 
-  constructor() { }
+  apiURL = environment.apiURLBoleta + 'boletas';
+
+  constructor(private http: HttpClient) { }
 }

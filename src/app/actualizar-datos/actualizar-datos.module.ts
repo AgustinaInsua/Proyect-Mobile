565746b73f75be956a8a0f05eeff7110b006d8d3
facundo <aguss.insua99@gmail.com>
2023-02-09 17:43:45 -0300

    first version actualizarDatos

--- src/app/actualizar-datos/actualizar-datos.module.ts
+++ src/app/actualizar-datos/actualizar-datos.module.ts
@@ -3,17 +3,30 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-
 import { ActualizarDatosPageRoutingModule } from './actualizar-datos-routing.module';
-
 import { ActualizarDatosPage } from './actualizar-datos.page';
+
+import { TableModule } from 'primeng/table';
+import { MenuModule } from './../menu/menu.module';
+import {ButtonModule} from 'primeng/button';
+import {AutoCompleteModule} from 'primeng/autocomplete';
+import {InputTextModule} from 'primeng/inputtext';
+import { HttpClientModule } from '@angular/common/http';
+import {CardModule} from 'primeng/card';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ActualizarDatosPageRoutingModule
+    ActualizarDatosPageRoutingModule,
+    TableModule,
+    MenuModule,
+    ButtonModule,
+    AutoCompleteModule,
+    InputTextModule,
+    HttpClientModule,
+    CardModule
   ],
   declarations: [ActualizarDatosPage]
 })

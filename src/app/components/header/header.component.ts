8a7bfb189acb45b9083b969b104170d3437173d8
facundo <aguss.insua99@gmail.com>
2023-02-27 12:59:11 -0300

    hover in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -13,13 +13,24 @@ export class HeaderComponent implements OnInit {
   @Input () icon!: any;
   es = Languages.es;
   en = Languages.en;
+  selectedEs !: boolean;
+  selectedEn !: boolean;
   constructor(private langService: LanguageService) { }
 
   ngOnInit() {
     console.log(this.langService.getActualLang());
   }
-  changeLang(lang: Languages){
+  changeLangEs(lang: Languages){
     this.langService.setLang(lang);
     console.log(this.langService.getActualLang());
+    this.selectedEs = true;
+    this.selectedEn = false;
+  }
+
+  changeLangEn(lang: Languages){
+    this.langService.setLang(lang);
+    console.log(this.langService.getActualLang());
+    this.selectedEn = true;
+    this.selectedEs = false;
   }
 }
--- src/app/service/company-service/company.service.ts
+++ src/app/service/company-service/company.service.ts
@@ -38,4 +38,5 @@ export class CompanyService {
   getCompanyInformationMail(){
     return this.mail;
   }
+  
 }

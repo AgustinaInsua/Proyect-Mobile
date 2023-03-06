f58678bcd0ee4feab7731359aa08a0bd3b7a74f5
AgustinaInsua <aguss.insua99@gmail.com>
2023-03-06 11:09:14 -0300

    creation object calculadora to use ngmodel

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Calculadora } from '../model/Calculadora';
 
 @Component({
   selector: 'app-home',
@@ -14,45 +15,14 @@ export class HomePage {
   selecAmountPayments: any;
   selecPeriodicity: any;
   isRequerid:boolean =true;
-  aprobadores:any;
-  amountPayments:any;
-  periodicity:any;
-  myForm!: FormGroup;
+  calculator!: Calculadora;
 
-  constructor() {}
+  constructor() {
+  this.calculator = new Calculadora();
+  }
     
   ngOnInit() {
-    this.minDate = new Date();
-    this.aprobadores = ["Gaston Alles", "Noelia Sosa"];
-    this.amountPayments = ["1","2","3","4","5","6","7","8","9","10","11","12"];
-    this.periodicity = ["15","30","60"];
-    this.myForm = new FormGroup ({
-      calendar: new FormControl('', [
-        Validators.required
-      ]),
-    pago: new FormControl('', [
-        Validators.required
-      ])
-    })
-  }   
-  get f(){
-    return this.myForm.controls;
-  } 
-  get calendar(){
-    return this.myForm.get('calendar');
   }
-  get pago(){
-    return this.myForm.get('calendar');
-  }
-
-  public isDirty(field: string): boolean {
-    return this.myForm.controls[field].dirty;
-  }
-
-  public isTouched(field: string): boolean {
-    return this.myForm.controls[field].touched;
-  }
-
 
   onSelect(event:any){
     console.log(event);
---
+++ src/app/model/Calculadora.ts
@@ -0,0 +1,11 @@
+
+export class Calculadora {
+    constructor (
+    public todayDate = new Date(),
+    public aprobadores = ["Gaston Alles", "Noelia Sosa"],
+    public amountPayments = ["1","2","3","4","5","6","7","8","9","10","11","12"],
+    public periodicity = ["15","30","60"]
+    ){}
+}
+
+
